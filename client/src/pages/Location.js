bad1d685500cea0773a584a833cdfa5d3f3b5201
Magdalena Perry <magperry16@gmail.com>
2022-04-30 22:20:55 -0400

    location, rentals added

---
+++ client/src/pages/Location.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Location = () => {
+
+
+  return (
+    <>
+    <main>
+      <h2>Yay! This works, location page is linked.</h2>
+
+    </main>
+    </>
+
+
+  );
+};
+
+export default Location;
