6d0d569040dec35057c86a16b0ec840fbc08ffe4
Magdalena Perry <magperry16@gmail.com>
2022-04-30 22:19:49 -0400

    cart added

---
+++ client/src/pages/Cart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Cart = () => {
+
+
+  return (
+    <>
+    <main>
+      <h2>Yay! This works, cart page is linked.</h2>
+
+    </main>
+    </>
+
+
+  );
+};
+
+export default Cart;
