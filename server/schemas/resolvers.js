290290144ca07a06bdc96b85e9f0827aead7aa2c
David Aylward <Davidayl14@outlook.com>
2022-05-03 20:48:59 -0400

    stripe typedefs, resolvers, 404 page and success page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Item, Location} = require('../models');
+const { User, Item, Location, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
@@ -22,6 +23,53 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
+    order: async (parent, { _id }, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'orders.items',
+          populate: 'location'
+        });
+
+        return user.orders.id(_id);
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ items: args.items });
+      const line_items = [];
+
+      const { items } = await order.populate('items');
+
+      for (let i = 0; i < items.length; i++) {
+        const item = await stripe.items.create({
+          name: items[i].name,
+          description: items[i].description,
+          images: [`${url}/images/${items[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          item: item.id,
+          unit_amount: items[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
+    }
   },
 
   Mutation: {
@@ -29,6 +77,18 @@ const resolvers = {
       const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
+    },
+    addOrder: async (parent, { items }, context) => {
+      console.log(context);
+      if (context.user) {
+        const order = new Order({ items });
+
+        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+
+        return order;
+      }
+
+      throw new AuthenticationError('Not logged in');
     },
     login: async (_, { email, password }) => {
       const user = await User.findOne({ email });
