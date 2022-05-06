fdc8f281b7551ce59baa92b7a4f39c1bcf010bca
Magdalena Perry <magperry16@gmail.com>
2022-05-06 13:30:51 -0400

    cart

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -40,9 +40,9 @@ const CartItem = ({ item }) => {
         <div class="row">
           <div class="col-md-12 my-4">
             <div class="row">
-              <div class="col-md-6">
+              <div class="col-md-6 flex-row space-between">
                 <img
-                  className="img-fluid" alt={item.name} src={"http://via.placeholder.com/300"} />
+                  className="img-fluid " alt={item.name} src={"http://via.placeholder.com/300"} />
               </div>
               <div class="col-md-6">
                     <div className="flex-row space-between">
@@ -73,56 +73,6 @@ const CartItem = ({ item }) => {
         </div>
       </div>
 
-
-
-
-
-
-
-
-      {/* <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-12 card mb-5 pt-5 mx-3 px-2">
-            <div className="row">
-              <div className="col-lg-3">
-                <img alt={item.name} src="http://via.placeholder.com/200" />
-                <div className="row">
-                  <div className="col-lg-12">
-                    <p>{item.name}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-5">
-                <p>{item.description}</p>
-                    <p className="text-success fw-bold px-1">${item.price}</p>
-              </div>
-
-
-              <div >
-              <span className="mt-2 border-0">Qty:</span>
-              <span>
-                <input
-                className="col-2 mx-2"
-                type="number"
-                placeholder="1"
-                value={item.purchaseQuantity}
-                onChange={onChange}
-              /></span>
-              </div>
-
-
-                <div className="flex-row space-between text-end">
-              <button
-                  className="btn btn-warning mb-5 mt-2 col-md-3 mx-2"
-                onClick={() => removeFromCart(item)}
-              >
-                Remove
-              </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
