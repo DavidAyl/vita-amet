87edc6194b91ca8c3a3abcb4e3acb3e786f8a6ad
Magdalena Perry <magperry16@gmail.com>
2022-05-07 01:32:29 -0400

    staged for pull

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/project',
+  process.env.MONGODB_URI || 'mongodb://localhost/vita-amet',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
