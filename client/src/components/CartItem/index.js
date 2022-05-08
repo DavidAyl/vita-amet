6ff7ab2c4616b540cc93ff6b4b4d866470901c31
David Aylward <Davidayl14@outlook.com>
2022-05-08 17:23:14 -0400

    staging for pull

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -34,7 +34,6 @@ const CartItem = ({ item }) => {
 
   return (
     <>
-<<<<<<< HEAD
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-12 card mb-5 pt-5 mx-3 px-2">
@@ -47,41 +46,30 @@ const CartItem = ({ item }) => {
                     <p className="text-success fw-bold px-1">${item.price}</p>
                   </div>
                 </div>
-=======
-
-
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-12 my-4">
-            <div class="row">
-              <div class="col-md-6 flex-row space-between">
-                <img
-                  className="img-fluid " alt={item.name} src={"http://via.placeholder.com/300"} />
->>>>>>> abf8bab0a6e561b2be50e0b65437d76138ce9ceb
               </div>
               <div class="col-md-6">
-                    <div className="flex-row space-between">
+                <div className="flex-row space-between">
                   <p><strong className="text-success">{item.name}</strong></p>
                   <p>{item.description}</p>
-                      <p><strong className="text-success">${item.price}</strong></p>
-                      <span className=" border-0">
-                        Qty:
-                        <input
-                          className=" col-3 mx-2"
-                          type="number"
-                          placeholder="1"
-                          value={item.purchaseQuantity}
-                          onChange={onChange}
-                        />
+                  <p><strong className="text-success">${item.price}</strong></p>
+                  <span className=" border-0">
+                    Qty:
+                    <input
+                      className=" col-3 mx-2"
+                      type="number"
+                      placeholder="1"
+                      value={item.purchaseQuantity}
+                      onChange={onChange}
+                    />
 
-                        <button
-                          className="btn btn-warning mx-2"
-                          onClick={() => removeFromCart(item)}
-                        >
-                          Remove
-                        </button>
+                    <button
+                      className="btn btn-warning mx-2"
+                      onClick={() => removeFromCart(item)}
+                    >
+                      Remove
+                    </button>
                   </span>
-                    </div>
+                </div>
               </div>
             </div>
           </div>
